cdb5ce5aeed2a717d213fc85cd3297c8d1505d13
Ilya Vorobyev <bjnhks@gmail.com>
2026-02-10 21:58:17 +0300

    Init

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import Header from './Header';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <div>Hello world</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div>
+      <button>Home</button>
+      <button>Sign up</button>
+      <button>Sign in</button>
+      <button>Sign out</button>
+      <button>Feature</button>
+    </div>
+  );
+};
+
+export default Header;
